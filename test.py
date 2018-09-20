cfd9bb55d2911d4692f4a49ecf298e2a3d0a2569
bang.wang <bang.wang@iim.ltd>
2018-09-20 10:27:47 +0800

    develop merge test

--- test.py
+++ test.py
@@ -18,15 +18,11 @@ develop: add a new line again = 5
 develop: add a new line again = 6 - 2018-09-20
 
 develop: add a new line again = 7 - 2018-09-20
-=======
+
 test: add a new line again = 3
 
 test: add a new line again = 4 - 2018-09-20
-<<<<<<< HEAD
->>>>>>> d850d75... commit test branch test00
-=======
 
 test: add a new line again = 5 - 2018-09-20
 
 test: add a new line again = 6 - 2018-09-20
->>>>>>> 258be42... commit test branch test02
